ccc4b0c66287e937ab07c3fa3040619b5f9f51c9
Federico Giorgino <federicog.dev@gmail.com>
2022-02-18 16:38:43 +0100

    Custom Theme

---
+++ src/types/*.d.ts
@@ -0,0 +1,24 @@
+import "@react-navigation/native";
+
+// Override the theme in react native navigation to accept our custom theme props.
+declare module "@react-navigation/native" {
+  export type ExtendedTheme = {
+    dark: boolean;
+    colors: {
+      primary: string;
+      secondary: string;
+      tertiary: string;
+      danger: string;
+      background: string;
+      card: string;
+      surface: string;
+      text: string;
+      subtext: string;
+      separator: string;
+      border: string;
+      highlight: string;
+      notification: string;
+    };
+  };
+  export function useTheme(): ExtendedTheme;
+}
