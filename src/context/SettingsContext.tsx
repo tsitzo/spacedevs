d5a3b022c8fe75bf22b96b03994328c7d236668d
Federico Giorgino <federicog.dev@gmail.com>
2022-02-18 17:12:26 +0100

    SettingsContext LaunchType

--- src/context/SettingsContext.tsx
+++ src/context/SettingsContext.tsx
@@ -1,25 +1,35 @@
 import React, { FC, createContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Themes } from "../types/settings";
+import { LaunchType, Themes } from "../types/settings";
 
 type SettingsContextState = {
   theme: Themes;
+  launchType: LaunchType;
   selectTheme: (theme: Themes) => void;
+  selectLaunchType: (launchType: LaunchType) => void;
 };
 
 const contextDefaultValue: SettingsContextState = {
   theme: "automatic",
+  launchType: "upcoming",
   selectTheme: () => {},
+  selectLaunchType: () => {},
 };
 export const SettingsContext =
   createContext<SettingsContextState>(contextDefaultValue);
 
 export const SettingsContextProvider: FC = ({ children }) => {
   const [theme, setTheme] = useState<Themes>(contextDefaultValue.theme);
+  const [launchType, setLaunchType] = useState<LaunchType>(
+    contextDefaultValue.launchType
+  );
 
   const selectTheme = (theme: Themes) => {
     setTheme(theme);
   };
+
+  const selectLaunchType = (launchType: LaunchType) =>
+    setLaunchType(launchType);
 
   const saveTheme = async (value: Themes) => {
     try {
@@ -41,16 +51,46 @@ export const SettingsContextProvider: FC = ({ children }) => {
     }
   };
 
+  const saveLaunchType = async (value: LaunchType) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@ORBITAL/launchType", jsonValue);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadLaunchType = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@ORBITAL/launchType");
+      if (value !== null) {
+        setLaunchType(JSON.parse(value));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     loadTheme();
+  }, []);
+
+  useEffect(() => {
+    loadLaunchType();
   }, []);
 
   useEffect(() => {
     saveTheme(theme);
   }, [theme]);
 
+  useEffect(() => {
+    saveLaunchType(launchType);
+  }, [launchType]);
+
   return (
-    <SettingsContext.Provider value={{ theme, selectTheme }}>
+    <SettingsContext.Provider
+      value={{ theme, launchType, selectTheme, selectLaunchType }}
+    >
       {children}
     </SettingsContext.Provider>
   );
--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,1 +1,5 @@
 export type Themes = "automatic" | "light" | "dark";
+
+export type LaunchType = "upcoming" | "previous";
+
+export type Browsers = "safari" | "inApp";
