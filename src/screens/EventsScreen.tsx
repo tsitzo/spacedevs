f329d6b36914d957df1c12fb2aa10feb8dea2995
Federico Giorgino <federicog.dev@gmail.com>
2022-02-25 15:28:02 +0100

    EventsScreen

--- src/screens/EventsScreen.tsx
+++ src/screens/EventsScreen.tsx
@@ -1,14 +1,94 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { FC, useLayoutEffect } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-const EventsScreen = () => {
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { SpaceEvent } from "../types/apiResponse";
+import { AppStackParams } from "../types/navigation";
+import { useTheme } from "@react-navigation/native";
+import { useFetch } from "../hooks/useFetch";
+import SafeArea from "../components/layout/SafeArea";
+import Spacer from "../components/layout/Spacer";
+import Typography from "../components/text/Typography";
+
+interface SpaceEventResponse {
+  count: number;
+  results: SpaceEvent[];
+}
+interface IEventsScreenProps {
+  navigation: NativeStackNavigationProp<AppStackParams, "AppTabs">;
+}
+
+const EventsScreen: FC<IEventsScreenProps> = ({ navigation }) => {
+  const { colors } = useTheme();
+  const URI = `https://lldev.thespacedevs.com/2.2.0/event/upcoming/?limit=30`;
+  const { response, error, loading, fetchData } =
+    useFetch<SpaceEventResponse>(URI);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.push("SavedEventsScreen")}>
+          <Ionicons name="bookmark" color={colors.primary} size={24} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>EventsScreen</Text>
-    </View>
+    <SafeArea>
+      {loading && (
+        <View style={styles.centeredPage}>
+          <ActivityIndicator size={60} color={colors.primary} />
+        </View>
+      )}
+      {error && !loading && (
+        <View style={styles.centeredPage}>
+          <TouchableOpacity onPress={() => fetchData()}>
+            <Ionicons name="refresh" color={colors.primary} size={40} />
+          </TouchableOpacity>
+          <Spacer y={10} />
+          <Typography>There was an error fetching data.</Typography>
+        </View>
+      )}
+      {response && !loading && !error && (
+        <FlatList
+          contentContainerStyle={styles.flatListContent}
+          data={response.results}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item, index }) => (
+            <View
+              style={[
+                styles.listItemWrapper,
+                {
+                  borderBottomColor: colors.separator,
+                  borderBottomWidth:
+                    index + 1 < response.results.length ? 0.17 : 0,
+                },
+              ]}
+            ></View>
+          )}
+        />
+      )}
+    </SafeArea>
   );
 };
 
 export default EventsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centeredPage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  listItemWrapper: { paddingVertical: 20 },
+  flatListContent: { paddingHorizontal: 15 },
+});
